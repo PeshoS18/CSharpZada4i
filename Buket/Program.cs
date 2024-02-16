70e6c7e5c7870f40837129718a6a41411060229e
PeshoS <109525686+PeshoS18@users.noreply.github.com>
2024-02-16 18:27:10 +0200

    Bileti/Tsvetya

---
+++ Buket/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Buket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double chervenaRoza = 7.89;
+            Console.WriteLine($"Цена за една червена роза:{chervenaRoza}");
+            double lale = 5.89;
+            Console.WriteLine($"Цена за едно лале:{lale}");
+            double orhideya = 45.32;
+            Console.WriteLine($"Цена за една орхидея: {orhideya}");
+            double aranzhirane = 5.00;
+            Console.WriteLine($"Цена за аранжиране на букет: {aranzhirane}");
+            Console.WriteLine("Брой червени рози:");
+            int broiCherveniRozi = int.Parse(Console.ReadLine());
+            Console.WriteLine("Брой лалета:");
+            int broiLaleta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Брой орхидеи:");
+            int broiOrhidei = int.Parse(Console.ReadLine());
+            double krainaSuma = 0;
+            int obshtBroiTsvetya = broiCherveniRozi + broiLaleta + broiOrhidei;
+
+            Console.WriteLine("Изберете повод/сезон: \n1-св.Валентин\n2-Пролет\n3-Лято");
+            int povod_sezon = int.Parse(Console.ReadLine());
+
+            switch (povod_sezon)
+            {
+                case 1:
+                    krainaSuma = (((broiCherveniRozi * chervenaRoza) + (broiLaleta * lale) + (broiOrhidei * orhideya)) + aranzhirane) * 0.85;
+                    if (obshtBroiTsvetya >= 22)
+                    {
+                        krainaSuma *= 0.77;
+                    }
+                    Console.WriteLine($"Крайна сума:{krainaSuma}");
+                    break;
+                case 2:
+                    krainaSuma = ((broiCherveniRozi * chervenaRoza) + (broiLaleta * lale) + (broiOrhidei * orhideya)) + aranzhirane;
+                    if (broiLaleta >= 3)
+                    {
+                        krainaSuma *= 0.95;
+                    }
+                    if (obshtBroiTsvetya >= 22)
+                    {
+                        krainaSuma *= 0.77;
+                    }
+                    Console.WriteLine($"Крайна сума:{krainaSuma}");
+                    break;
+                case 3:
+                    krainaSuma = ((broiCherveniRozi * chervenaRoza) + (broiLaleta * lale) + (broiOrhidei * orhideya)) + aranzhirane;
+                    if (broiCherveniRozi == 12)
+                    {
+                        krainaSuma *= 0.90;
+                    }
+                    if (obshtBroiTsvetya >= 22)
+                    {
+                        krainaSuma *= 0.77;
+                    }
+                    Console.WriteLine($"Крайна сума:{krainaSuma}");
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    }
+}
---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tsenaZaVIPBileti = 497.54;
+            Console.WriteLine($"Цена на VIP билети:{tsenaZaVIPBileti}");
+            double tsenaZaNormalniBileti = 245.80;
+            Console.WriteLine($"Цена на нормални билети:{tsenaZaNormalniBileti}");
+            Console.WriteLine("Брой хора:");
+            int hora = int.Parse(Console.ReadLine());
+            Console.WriteLine("Бюджет:");
+            double byudzhet = double.Parse(Console.ReadLine());
+            double byudzhetSledPreravnyavane = 0;
+
+            if (hora >= 1 && hora <= 4)
+            {
+                byudzhetSledPreravnyavane = byudzhet * 0.75;
+
+            }
+            else if (hora >= 5 && hora <= 9)
+            {
+                byudzhetSledPreravnyavane = byudzhet * 0.60;
+            }
+            else if (hora >= 10 && hora <= 24)
+            {
+                byudzhetSledPreravnyavane = byudzhet * 0.50;
+            }
+            else if (hora >= 25 && hora <= 49)
+            {
+                byudzhetSledPreravnyavane = byudzhet * 0.40;
+            }
+            else if (hora > 50)
+            {
+                byudzhetSledPreravnyavane = byudzhet * 0.25;
+            }
+            else
+            {
+                Console.WriteLine("Няма хора");
+
+            }
+
+            Console.WriteLine($"Крайни пари:{byudzhetSledPreravnyavane}");
+
+            if (byudzhetSledPreravnyavane >= tsenaZaVIPBileti)
+            {
+                Console.WriteLine("Парите стигат за VIP!");
+            }
+            else if (byudzhetSledPreravnyavane <= tsenaZaVIPBileti)
+            {
+                Console.WriteLine("Не стигат парите за VIP!");
+                Console.WriteLine("Има достатъчно пари за нормални билети!");
+            }
+            else
+            {
+                Console.WriteLine("Няма достатъчно пари за нормални билети!");
+            }
+        }
+    }
+}

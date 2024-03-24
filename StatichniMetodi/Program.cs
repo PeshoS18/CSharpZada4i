21b31be2d4074f4be130bcca4993fd088c680e81
PeshoS <109525686+PeshoS18@users.noreply.github.com>
2024-03-24 16:09:13 +0200

    StatichniMetodi

---
+++ StatichniMetodi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StatichniMetodi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TochkaVRavnina tochka1 = new TochkaVRavnina();
+            TochkaVRavnina tochka2 = new TochkaVRavnina();
+
+            tochka1.Input();
+            tochka1.Output();
+            tochka2.Input();
+
+            double l = TochkaVRavnina.Distance(tochka1, tochka2);
+            Console.WriteLine($"Нещо:{l}");
+        }
+    }
+}
---
+++ StatichniMetodi/TochkaVRavnina.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatichniMetodi
+{
+    public class TochkaVRavnina
+    {
+        private int x, y;
+
+        public void Input()
+        {
+            Console.Write("Въведете x:");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Въведете y:");
+            y = int.Parse(Console.ReadLine());
+        }
+        public void Output()
+        {
+            Console.WriteLine($"({x}, {y})");
+        }
+
+        public static double Distance(TochkaVRavnina nachalo, TochkaVRavnina krai)
+        {
+            int dnachalo, dkrai;
+            dnachalo = nachalo.x - krai.x;
+            dkrai = nachalo.y - krai.y;
+            double l = Math.Sqrt(dnachalo * dnachalo + dkrai * dkrai);
+            return l;
+        }
+    }
+}
